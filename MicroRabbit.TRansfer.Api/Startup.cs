25ed81169ffe8d1ae34a4c63ad1eb1a68a61e7a0
VyasVijay1 <learning.curve.42@gmail.com>
2020-01-06 22:52:25 -0700

    Initial Commit for Transfer MicroService Projects

--- MicroRabbit.TRansfer.Api/Startup.cs
+++ MicroRabbit.TRansfer.Api/Startup.cs
@@ -1,18 +1,17 @@
-﻿using MediatR;
-using MicroRabbit.Banking.Data.Context;
-using MicroRabbit.Infra.IoC;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 
-
-namespace MicroRabbit.Banking.Api
+namespace MicroRabbit.TRansfer.Api
 {
 	public class Startup
 	{
@@ -26,23 +25,7 @@ namespace MicroRabbit.Banking.Api
 		// This method gets called by the runtime. Use this method to add services to the container.
 		public void ConfigureServices(IServiceCollection services)
 		{
-			services.AddDbContext<BankingDbContext>(options =>
-			{
-				options.UseSqlServer(Configuration.GetConnectionString("BankingDbConnection"));
-			});
 			services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
-			services.AddSwaggerGen(c =>
-			{
-				c.SwaggerDoc("v1", new Swashbuckle.AspNetCore.Swagger.Info { Title = "Banking Microservice", Version = "v1" });
-			});
-
-			//services.AddMediatR(typeof(Startup));
-			RegisterServices(services);
-		}
-
-		private void RegisterServices(IServiceCollection services)
-		{
-			DependencyContainer.RegisterServices(services);
 		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -58,13 +41,6 @@ namespace MicroRabbit.Banking.Api
 			}
 
 			app.UseHttpsRedirection();
-
-			app.UseSwagger();
-			app.UseSwaggerUI(
-				c => {
-					c.SwaggerEndpoint("/swagger/v1/swagger.json", "Banking Microsevice V1");
-				}
-				);
 			app.UseMvc();
 		}
 	}
